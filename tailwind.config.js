5301dcd049123f28d561c7a5b543c76a55e107ec
Garen <damienlaus@icloud.com>
2021-08-27 16:25:02 +0800

    chore: import tailwind-css plugin.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html", "./website/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
