ce39146abf13374d4037bcb80e12445a7461639a
Garen <damienlaus@icloud.com>
2021-08-30 21:23:01 +0800

    feat: global empty.

--- website/App.tsx
+++ website/App.tsx
@@ -1,12 +1,22 @@
+import { ConfigProvider, Empty } from "ant-design-vue";
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import zhCN from "ant-design-vue/es/locale/zh_CN";
 
 export default defineComponent({
   setup() {
+    const emptyRender = {
+      renderEmpty: () => (
+        <>
+          <Empty image="/icon_empty_data.png" />
+        </>
+      ),
+    };
+
     return () => (
-      <>
+      <ConfigProvider locale={zhCN} v-slots={emptyRender}>
         <RouterView />
-      </>
+      </ConfigProvider>
     );
   },
 });
