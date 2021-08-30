a5c372381d1a4bab588e865f7f82b835982ac73e
Garen <damienlaus@icloud.com>
2021-08-30 21:28:04 +0800

    feat: expand shortcut.

--- website/pages/warehouse/shortcut/index.tsx
+++ website/pages/warehouse/shortcut/index.tsx
@@ -1,25 +1,36 @@
-import { defineComponent, ref } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import { Button, Space } from "ant-design-vue";
+import { useStore } from "vuex";
+import List from "components/List";
 import Tabs from "components/Tabs";
 
 export default defineComponent({
   setup() {
+    const store = useStore();
     const tabColumns = ref([
       {
         label: "借货清单",
         key: "1",
+        count: computed(() => store.state.warehouse.shortcut.total.lend),
       },
       {
         label: "维修清单",
         key: "2",
+        count: computed(() => store.state.warehouse.shortcut.total.repair),
       },
       {
         label: "保养清单",
         key: "3",
+        count: computed(() => store.state.warehouse.shortcut.total.maintain),
       },
     ]);
+    const cardListsData = ref({});
 
-    const handleClickTabPane = (active: object) => {};
+    const handleClickTabPane = (active: object) => {
+      store.dispatch("warehouse/shortcut/getLists").then((response) => {
+        cardListsData.value = response;
+      });
+    };
 
     return () => (
       <Tabs
@@ -38,6 +49,23 @@ export default defineComponent({
               </Space>
             );
           },
+          default: () => (
+            <List grid={5} class="h-full" dataSource={cardListsData.value}>
+              {{
+                card: (item) => (
+                  <>
+                    {console.log(item)}
+                    <p>
+                      <span>{item.item.createMan}</span>
+                    </p>
+                    <p>
+                      <span>{item.index}</span>
+                    </p>
+                  </>
+                ),
+              }}
+            </List>
+          ),
         }}
       </Tabs>
     );
--- website/store/modules/warehouse/shortcut.ts
+++ website/store/modules/warehouse/shortcut.ts
@@ -1,10 +1,51 @@
-const state = () => ({});
+import {
+  findShortcutCountData,
+  findSpecifiedShortcutData,
+} from "api/warehouse/shortcut";
+
+const state = () => ({
+  total: { all: 0, maintain: 0, repair: 0, lend: 0 },
+});
 
 const getters = {};
 
-const actions = {};
+const actions = {
+  // 获取待操作清单数量
+  getTotals: ({ commit }) => {
+    return new Promise<void>((reslove) => {
+      findShortcutCountData().then((count) => {
+        commit("SET_TOTAL", {
+          all: count.totalNum,
+          maintain: count.baoYangNum,
+          repair: count.weiXiuNum,
+          lend: count.outNum,
+        });
+        reslove();
+      });
+    });
+  },
 
-const mutations = {};
+  // 获取待操作清单列表
+  getLists: ({ dispatch }) => {
+    return new Promise((reslove) => {
+      dispatch("getTotals").then(() => {
+        findSpecifiedShortcutData().then((response) => {
+          reslove(response);
+        });
+      });
+    });
+  },
+};
+
+const mutations = {
+  SET_TOTAL: (state: State, totals: State) => {
+    state.total = totals;
+  },
+};
+
+export interface State {
+  total: { all: number; maintain: number; repair: number; lend: number };
+}
 
 export default {
   namespaced: true,
